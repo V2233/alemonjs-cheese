411efc0d9793d0359d9c400bd89885cf9895b879
Small-v <2330660495@qq.com>
2025-01-13 12:26:40 +0800

    fix: linux端mermaid脚本运行失败

--- src/utils/marked.ts
+++ src/utils/marked.ts
@@ -68,7 +68,45 @@ export async function toMermaid<T extends keyof IMermaidFormat>(text:string,outF
     const realPath = join(pluginInfo.DATA_PATH,`mermaid-1.${outFormat?outFormat:'svg'}`) 
     writeFileSync(inputPath,text,'utf-8')
     await run(
-        inputPath, outputPath, // {optional options},
+        inputPath, 
+        outputPath,
+        {
+            puppeteerConfig: {
+                timeout: 0, //otocolTimeout: 0,
+                headless: true,
+                args: [
+                    '--disable-dev-shm-usage', // 禁用 /dev/shm 的使用，防止共享内存不足的问题
+                    '--disable-setuid-sandbox', // 禁用 setuid 沙盒
+                    '--no-first-run', // 跳过首次运行的设置
+                    '--no-sandbox', // 禁用沙盒模式
+                    '--no-zygote', // 禁用 zygote 进程
+                    '--single-process', // 使浏览器在单个进程中运行
+                    '--disable-background-networking', // 禁用后台网络请求
+                    '--disable-background-timer-throttling', // 禁用后台计时器节流
+                    '--disable-backgrounding-occluded-windows', // 禁用后台窗口
+                    '--disable-breakpad', // 禁用崩溃报告
+                    '--disable-client-side-phishing-detection', // 禁用客户端钓鱼检测
+                    '--disable-component-update', // 禁用组件更新
+                    '--disable-default-apps', // 禁用默认应用
+                    '--disable-domain-reliability', // 禁用域名可靠性
+                    '--disable-extensions', // 禁用扩展
+                    '--disable-features=AudioServiceOutOfProcess', // 禁用音频服务进程外处理
+                    '--disable-hang-monitor', // 禁用挂起监视器
+                    '--disable-ipc-flooding-protection', // 禁用 IPC 洪水保护
+                    '--disable-popup-blocking', // 禁用弹出窗口阻止
+                    '--disable-print-preview', // 禁用打印预览
+                    '--disable-prompt-on-repost', // 禁用重新发布提示
+                    '--disable-renderer-backgrounding', // 禁用渲染器后台处理
+                    '--disable-sync', // 禁用同步
+                    '--force-color-profile=srgb', // 强制使用 sRGB 颜色配置文件
+                    '--metrics-recording-only', // 仅记录指标
+                    '--safebrowsing-disable-auto-update', // 禁用安全浏览自动更新
+                    '--enable-automation', // 启用自动化
+                    '--password-store=basic', // 使用基本密码存储
+                    '--use-mock-keychain' // 使用模拟密钥链
+                ]
+            }
+        }
     )
     let buffer: IMermaidFormat[T]
 
